717c50e2e18e9e2ea6df34e13f82ab6683e9d78a
Sniklz <tuturox91@gmail.com>
2025-07-05 22:24:42 +0200

    finish-in-2025

--- src/main/java/core/basesyntax/Engine.java
+++ src/main/java/core/basesyntax/Engine.java
@@ -1,10 +1,48 @@
 package core.basesyntax;
 
-public class Engine {
+import java.util.Objects;
+
+public class Engine implements Cloneable {
     private int horsePower;
     private String manufacturer;
 
     //implement this class
+    public Engine(int horsePower, String manufacturer) {
+        this.horsePower = horsePower;
+        this.manufacturer = manufacturer;
+    }
+
+    public void setHorsePower(int horsePower) {
+        this.horsePower = horsePower;
+    }
+
+    public void setManufacturer(String manufacturer) {
+        this.manufacturer = manufacturer;
+    }
+
+    public int getHorsePower() {
+        return horsePower;
+    }
+
+    public String getManufacturer() {
+        return manufacturer;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(horsePower, manufacturer);
+    }
+
+    @Override
+    protected Engine clone() {
+        Engine engine;
+        try {
+            engine = (Engine) super.clone();
+        } catch (CloneNotSupportedException e) {
+            throw new RuntimeException(e);
+        }
+        return engine;
+    }
 
     @Override
     public String toString() {
@@ -12,5 +50,15 @@ public class Engine {
             + "horsePower=" + horsePower
             + ", manufacturer='" + manufacturer + '\''
             + '}';
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (!(obj instanceof Engine)) {
+            return false;
+        }
+        Engine engine = (Engine) obj;
+        return Objects.equals(horsePower, engine.horsePower)
+                && Objects.equals(manufacturer, engine.manufacturer);
     }
 }
--- src/main/java/core/basesyntax/Wheel.java
+++ src/main/java/core/basesyntax/Wheel.java
@@ -1,14 +1,52 @@
 package core.basesyntax;
 
-public class Wheel {
+import java.util.Objects;
+
+public class Wheel implements Cloneable {
     private int radius;
 
     //implement this class
+    public Wheel(int radius) {
+        this.radius = radius;
+    }
+
+    public void setRadius(int radius) {
+        this.radius = radius;
+    }
+
+    public int getRadius() {
+        return radius;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(radius);
+    }
+
+    @Override
+    protected Wheel clone() {
+        Wheel wheel;
+        try {
+            wheel = (Wheel) super.clone();
+        } catch (CloneNotSupportedException e) {
+            throw new RuntimeException(e);
+        }
+        return wheel;
+    }
 
     @Override
     public String toString() {
         return "Wheel{"
-            + "radius=" + radius
-            + '}';
+                + "radius=" + radius
+                + '}';
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (!(obj instanceof Wheel)) {
+            return false;
+        }
+        Wheel wheel = (Wheel) obj;
+        return radius == wheel.radius;
     }
 }
